c357e8841436853968d870766a6ae4005269bb58
Shuvoghosh7 <shuvogosh1610@gmail.com>
2024-10-30 15:16:03 +0600

    banner part done

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,11 @@
 import React from "react"
 import RootLayout from "../component/Layout/RootLayout";
+import Banner from "@/component/UI/Banner";
 
 const HomePage = (props) => {
   return (
     <div>
-      <h1>This is Home page</h1>
+      <Banner/>
     </div>
   )
 };
