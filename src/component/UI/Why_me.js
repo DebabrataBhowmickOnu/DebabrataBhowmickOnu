ef435fea9492c982792236c4a1049d671a6fe47c
Shuvoghosh7 <shuvogosh1610@gmail.com>
2024-10-31 18:30:50 +0600

    Fix for deploay3

--- src/component/UI/Why_me.js
+++ src/component/UI/Why_me.js
@@ -1,16 +1,17 @@
 import Image from "next/image";
-import React from "react"
-import whyme from '../../assets/images/whyme.png'
+import React from "react";
+import whyme from '../../assets/images/whyme.png';
 import styles from "@/styles/Whyme.module.css";
-const Whyme = (props) => {
+
+const Whyme = () => {
     return (
         <div className={styles.whyme_container} id="Why_Me">
             <div className={styles.Whyme_content}>
                 <h2>Why me?</h2>
                 <p className={styles.frist_content}>Hi There! I am Debabrata Onu</p>
-                <p>A Freelance Full Stack Web Developer with more than 4 Years of experience. I thoughtfully craft brand-focused design expertly build responsive and well-functional websites with great user experiences. Check out my latest projects! Lets chat and drive more conversions together!
+                <p>
+                    A Freelance Full Stack Web Developer with more than 4 Years of experience. I thoughtfully craft brand-focused design, expertly build responsive and well-functional websites with great user experiences. Check out my latest projects! Let&apos;s chat and drive more conversions together!
                 </p>
-
             </div>
             <div>
                 <Image
@@ -20,9 +21,8 @@ const Whyme = (props) => {
                     alt="Picture of the logo"
                 />
             </div>
-
         </div>
-    )
+    );
 };
 
 export default Whyme;
