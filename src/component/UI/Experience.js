395865dcba33f570ed3b24c404d628a1a8bfed3c
Shuvoghosh7 <shuvogosh1610@gmail.com>
2024-10-31 18:13:10 +0600

    Done slider section2

--- src/component/UI/Experience.js
+++ src/component/UI/Experience.js
@@ -20,14 +20,14 @@ const Experience = () => {
                 <div className={styles.experience1}>
                     <h3><span>Angular Developer | Resultier |</span> April 2022 - December 2022 </h3>
                     <p>
-                        I worked, with a very diverse team of brilliant developers, on a cutting-edge health-tech application built using latest Angular versions and took charge of crafting and solving intricate frontend logic. Notably, I successfully tackled complex data-table and form interactions, including the challenging HCFA, contributing to the application's seamless and efficient user experience.
+                        I worked, with a very diverse team of brilliant developers on a cutting-edge health-tech application built using latest Angular versions and took charge of crafting and solving intricate frontend logic. Notably, I successfully tackled complex data-table and form interactions, including the challenging HCFA contributing to the applications seamless and efficient user experience.
                     </p>
                 </div>
 
                 <div className={styles.experience2}>
                     <h3><span>Angular Developer | Resultier | </span> April 2022 - December 2022 </h3>
                     <p>
-                        I worked, with a very diverse team of brilliant developers, on a cutting-edge health-tech application built using latest Angular versions and took charge of crafting and solving intricate frontend logic. Notably, I successfully tackled complex data-table and form interactions, including the challenging HCFA, contributing to the application's seamless and efficient user experience.
+                        I worked with a very diverse team of brilliant developers on a cutting-edge health-tech application built using latest Angular versions and took charge of crafting and solving intricate frontend logic. Notably I successfully tackled complex data-table and form interactions including the challenging HCFA contributing to the applications seamless and efficient user experience.
                     </p>
                 </div>
             </div>
